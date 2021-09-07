60c7c4e08f6998772bf0000381e2f34cd1144e90
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2021-09-07 23:44:14 +0200

    adding the project

---
+++ src/test/java/com/example/restapimyfriendudemy/RestApiMyFriendUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapimyfriendudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMyFriendUdemyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
