4d34c0e53474301e4bddc379cb64a0c00883095b
Niegowski Tomasz-PNM386 <Tomasz.Niegowski@motorolasolutions.com>
2021-09-09 20:58:53 +0200

    rest api from udemy until lesson 15

--- src/main/java/com/example/restapimyfriendudemy/controller/StudentController.java
+++ src/main/java/com/example/restapimyfriendudemy/controller/StudentController.java
@@ -1,2 +1,22 @@
-package com.example.restapimyfriendudemy.controller;public class StudentController {
+package com.example.restapimyfriendudemy.controller;
+
+import com.example.restapimyfriendudemy.response.StudentResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/student/")
+public class StudentController {
+
+  @Value("${app.name:Default Demo App}")
+  private String appName;
+
+  @GetMapping("/get")
+  public StudentResponse getStudent() {
+    StudentResponse studentResponse = new StudentResponse(1, "John", "Smith");
+    return studentResponse;
+  }
+
 }
